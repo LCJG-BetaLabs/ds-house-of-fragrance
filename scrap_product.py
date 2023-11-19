d824b2d0cd7223879fa06878234c77124c545100
seanchan-lcjg <135810170+seanchan-lcjg@users.noreply.github.com>
2023-11-19 15:49:50 +0800

    Update scrap_product.py comments

--- scrap_product.py
+++ scrap_product.py
@@ -13,22 +13,30 @@ def scraping_with_retry():
             base_url = 'https://www.fragrantica.com/'
             brand_csv = pd.read_csv('brand_urls.csv')
 
+            
+            # Get each brand and scrape product in the brand
             for i in range(len(brand_csv['brand'])):
                 brand = brand_csv['brand'][i]
+
+                # Check if the folder for the brand already existed, create it if not
                 if not os.path.exists(brand):
                     os.mkdir(brand)
                 print(f"Start scraping on {brand}")
 
-
+                # Load the prodcuts' url of the brand
                 with open(f'brand_json/{brand}.json', 'r', encoding='utf-8') as file:
                     prod_brand_urls = json.load(file)
 
 
                 scraping_no = 1
+
+                # Get each product's url and scrape
                 for prod_url in prod_brand_urls:
                     url = base_url + prod_url
-
                     prod_url_name = prod_url.split("/")[-1].replace(".html", "")
+
+                    
+                    # check if the product already scraped, skip it if yes
                     if os.path.exists(f'{brand}/{brand}_{prod_url_name}_prod_dict.json'):
                         # print(f'{brand}/{brand}_{prod_url_name}_prod_dict.json')
                         scraping_no += 1
@@ -36,48 +44,23 @@ def scraping_with_retry():
                     else:
                         print(f"{scraping_no}/{len(prod_brand_urls)} of {brand}")
 
-                    # code for scrapperapi scrap
-                    # response = requests.get('http://api.scraperapi.com',
-                    # {
-                    #         'api_key': '03cb768180a77fe3746ee367fa787d4e',
-                    #         'url': url,
-                    #         'render': 'true'
-                    #  }
-                    # )
-
-                    # code for abstract api to scrap
-                    # response = requests.get('https://app.zenscrape.com/api/v1/get',
-                    #                         headers={
-                    #                             "apikey": "f9e1fad0-855b-11ee-909e-bbe99a7064b7"
-                    #                         },
-                    #                         params = (
-                    #                            ("url",url),
-                    #                            ("render", "true"),
-                    #                            ("wait_for", "1"),
-                    #                         )
-                    #                         )
-
-                    # code for scrapeops.io scrap
-
+                    
+                    # code for scrapeops.io api
                     response = requests.get(
                         url='https://proxy.scrapeops.io/v1/',
                         params={
-                            # 'api_key': "dcfbb9a9-56d1-4085-a3be-2291a907d570",
                             'api_key': "b33560d5-7b3c-4606-bdb3-ce3d82de97ed",
-                            # 'api_key': "b58d9e17-a2e0-416e-a049-1f9493b4558f",
-                            # 'api_key': "107ae881-59de-41e2-9640-c68c311a1c16",
-                            # 'api_key': "add1bd93-1246-48a7-8706-d7668ba737ca",
-                            # 'api_key': "cb8df83b-4863-4662-9625-453d0f7769fd",
-                            # 'api_key': "d0537dde-9616-4f45-ac59-0d8aa976c3d7",
-                            # 'api_key': "84f0227b-09a5-4fdf-8d97-123aa2ef3160",
                             'url': url,
-                            'render_js': 'true',
+                            'render_js': 'true', # add java rendering
                         },
                     )
+
+                    # Create a new BeautifulSoup object from the HTML content
                     soup = BeautifulSoup(response.text, 'html.parser')
 
                     list_of_perfume_dicts = []
-                    # Create a new BeautifulSoup object from the HTML content
+
+                    # Get name, company, image and for gender of the product
                     try:
                         perfume_name = soup.find_all("div", class_="cell small-12")[3].find_all("b")[0].get_text()
                         perfume_comp = soup.find_all("div", class_="cell small-12")[3].find_all("b")[1].get_text()
@@ -86,26 +69,29 @@ def scraping_with_retry():
                         print(perfume_name)
                     except:
                         print(soup.contents)
+                        # Check if api credits are all used or get banned from the api, if yes stop the program
                         if "banned" in soup.contents[0] or "API" in soup.contents[0]:
                             exit()
 
-
+                    # Get rating number and number of votes
                     try:
                         rating = float(soup.find("p", class_="info-note").find_all("span")[0].get_text())
                         number_votes = int(soup.find("p", class_="info-note").find_all("span")[2].get_text().replace(',', ''))
                     except:
                         rating = "NA"
                         number_votes = "NA"
-                        print(f"{perfume_name} does not have a ranking")
+                        print(f"{perfume_name} does not have a rating")
 
+                    # Get description
                     try:
                         description = soup.find_all("div", class_="cell small-12")[3].find("p").get_text()
                         description = re.sub(r'\s+', ' ',description.strip())
                     except:
                         description = "NA"
                         print(f"{perfume_name} does not have a description")
-                    ####### MAIN ACCORDS DICTIONARY #######
+                    
 
+                    # Get main accords dictionary
                     try:
                         main_accords = soup.find_all("div", class_="cell accord-box")
                         accords_dict = {}
@@ -117,7 +103,7 @@ def scraping_with_retry():
                         accords_dict = {}
                         print(f"{perfume_name} does not have accords")
 
-                    ####### FRAGRANCE NOTES #######
+                    # Get fragrance notes
                     notes = soup.find_all("div", attrs={"style": "display: flex; flex-direction: column; justify-content: center; text-align: center; background: white;"})
                     top_notes_list = []
                     middle_notes_list = []
@@ -132,6 +118,8 @@ def scraping_with_retry():
                                     note_element = note_element.find_next_sibling("div")
                                 except:
                                     break
+                                    
+                        # case if the product don't have top, middle and base notes
                         else:
                             note_index = 0
                             for note in notes[0].find_all("h4"):
@@ -147,7 +135,9 @@ def scraping_with_retry():
                                 note_index += 1
                     except:
                         print(f"{perfume_name} does not have notes")
-                    ####### VOTING DATA & INFORMATION #######
+
+                    
+                    # Get voting data
                     voting = soup.find_all("div", class_="cell small-1 medium-1 large-1")
 
                     ####### Longevity #######
@@ -176,9 +166,8 @@ def scraping_with_retry():
                     value_ok = int(voting[16].get_text())
                     value_good = int(voting[17].get_text())
                     value_great = int(voting[18].get_text())
-                    value_great = int(voting[18].get_text())
 
-                    ####### CREATING THE DICTIONARY OF DATA #######
+                    # dictionary for each product
                     perfume_dict = {"name": perfume_name,
                                     "company": perfume_comp,
                                     "image": perfume_image,
@@ -212,13 +201,17 @@ def scraping_with_retry():
                                    }
                     scraping_no += 1
                     list_of_perfume_dicts.append(perfume_dict)
-                    # time.sleep(2)
+
+                    # save each product dictionary in the corresponding brand
                     with open(f'{brand}/{brand}_{prod_url_name}_prod_dict.json', 'w') as file:
                         json.dump(perfume_dict, file, indent=4)
                         print(f"Finish scraping on {brand}_{prod_url_name}")
+
+                
                 print(f"Finish scraping on {brand}")
                 print("---------------------------------")
-
+                
+        # retry in two seconds if connection issue appeared
         except Exception as e:
             print(f"An error occurred: {e}")
             print(f"Retrying in {retry_delay} seconds...")
@@ -228,5 +221,5 @@ def scraping_with_retry():
             # If no exception occurred, exit the loop and function
             return
 
-#Call the function to run code with retry
+# Call the function to run code with retry
 scraping_with_retry()
