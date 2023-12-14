d673edf5a987119e90b76bfe650303fb19bc7b9b
cintiaching <cintiaching>
2023-12-14 02:39:54 +0000

    modify profiling

--- profiling_features.py
+++ profiling_features.py
@@ -1,4 +1,11 @@
 # Databricks notebook source
+dbutils.widgets.removeAll()
+dbutils.widgets.text("result_path", "")
+
+result_path = getArgument("result_path")
+
+# COMMAND ----------
+
 import os
 import pyspark.sql.functions as f
 
@@ -6,28 +13,22 @@ from utils.enviroment import BASE_DIR, LC_FRAGRANTICA_MATCHING
 
 # COMMAND ----------
 
+# MAGIC %run ./notes_grouping
+
+# COMMAND ----------
+
 model_dir = os.path.join(BASE_DIR, "model")
-result = spark.read.parquet(os.path.join(model_dir.replace("/dbfs", ""), "clustering_result.parquet"))
+result = spark.read.parquet(result_path)
 matching = spark.table(LC_FRAGRANTICA_MATCHING)
 
 # COMMAND ----------
 
 result = result.withColumn("cluster", f.concat(f.lit("cluster "), f.col("cluster")))
+result = result.withColumn("dummy", f.lit(1))
 
 # COMMAND ----------
 
-display(result)
-
-# COMMAND ----------
-
-display(matching)
-
-# COMMAND ----------
-
-final_df = matching.join(result, on="atg_code", how="left")
-
-# COMMAND ----------
-
+final_df = matching.join(result, on="atg_code", how="inner")
 final_df.createOrReplaceTempView("final_df")
 
 # COMMAND ----------
@@ -53,6 +54,10 @@ def count_pivot_table(table, group_by_col, agg_col):
 
 # COMMAND ----------
 
+count_pivot_table(final_df, "dummy", "atg_code")
+
+# COMMAND ----------
+
 count_pivot_table(final_df, "color_desc", "atg_code")
 
 # COMMAND ----------
@@ -69,17 +74,20 @@ count_pivot_table(final_df, "brand_desc", "atg_code")
 
 # COMMAND ----------
 
-# price point (H, M, L from percentile)
-
-
-# COMMAND ----------
-
 count_pivot_table(final_df, "for_gender", "atg_code")
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC SELECT atg_code, top_notes, middle_notes, base_notes FROM final_df
+from pyspark.sql.functions import udf
+from pyspark.sql.types import StringType
+
+def group_notes_udf(mapping):
+    def group_notes(note):
+        for k, v in mapping.items():
+            if note in v:
+                return k
+
+    return udf(group_notes, StringType())
 
 # COMMAND ----------
 
@@ -90,6 +98,12 @@ count_pivot_table(df, "base_notes", "atg_code")
 
 # COMMAND ----------
 
+udf_group_notes = group_notes_udf(base_notes_mapping)
+df = df.withColumn("group", udf_group_notes(df["base_notes"]))
+count_pivot_table(df, "group", "atg_code")
+
+# COMMAND ----------
+
 # base/middle/top notes
 df = spark.sql("SELECT atg_code, middle_notes, cluster FROM final_df")
 df = df.withColumn("middle_notes", f.explode(f.col("middle_notes")))
@@ -97,10 +111,22 @@ count_pivot_table(df, "middle_notes", "atg_code")
 
 # COMMAND ----------
 
+udf_group_notes = group_notes_udf(middle_notes_mapping)
+df = df.withColumn("group", udf_group_notes(df["middle_notes"]))
+count_pivot_table(df, "group", "atg_code")
+
+# COMMAND ----------
+
 # base/middle/top notes
 df = spark.sql("SELECT atg_code, top_notes, cluster FROM final_df")
 df = df.withColumn("top_notes", f.explode(f.col("top_notes")))
 count_pivot_table(df, "top_notes", "atg_code")
+
+# COMMAND ----------
+
+udf_group_notes = group_notes_udf(top_notes_mapping)
+df = df.withColumn("group", udf_group_notes(df["top_notes"]))
+count_pivot_table(df, "group", "atg_code")
 
 # COMMAND ----------
 
@@ -115,13 +141,25 @@ count_pivot_table(spark.createDataFrame(df_exploded), "main_accords", "atg_code"
 
 # COMMAND ----------
 
-# process voting data
-# show the one with highest vote
+df = spark.sql("SELECT atg_code, main_accords, cluster FROM final_df").toPandas()
+
+def group_accords(accords):
+    for k, v in main_accords_grouping.items():
+        if accords in v:
+            return k
+        
+def get_max_dict_key(d):
+    d = {key: value if value is not None else 0 for key, value in d.items()}
+    return max(d, key=d.get)
+    
+df["main_accords"] = df["main_accords"].apply(lambda x: get_max_dict_key(x))
+df["main_accords"] = df["main_accords"].apply(lambda x: group_accords(x))
+count_pivot_table(spark.createDataFrame(df), "main_accords", "atg_code")
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC SELECT atg_code, season_rating, cluster FROM final_df
+# process voting data
+# show the one with highest vote
 
 # COMMAND ----------
 
@@ -153,7 +191,7 @@ count_pivot_table(spark.createDataFrame(df), "season", "atg_code")
 
 # sillage & longevity
 
-df = spark.sql("SELECT atg_code, sillage, longevity, cluster FROM final_df").toPandas()
+df = spark.sql("SELECT atg_code, sillage, longevity, price_value, cluster FROM final_df").toPandas()
 
 def get_max_dict_key(d):
     d = {key: value for key, value in d.items()}
@@ -161,6 +199,7 @@ def get_max_dict_key(d):
 
 df["sillage"] = df["sillage"].apply(lambda d: get_max_dict_key(d))
 df["longevity"] = df["longevity"].apply(lambda d: get_max_dict_key(d))
+df["price_value"] = df["price_value"].apply(lambda d: get_max_dict_key(d))
 
 # COMMAND ----------
 
@@ -169,6 +208,10 @@ count_pivot_table(spark.createDataFrame(df), "sillage", "atg_code")
 # COMMAND ----------
 
 count_pivot_table(spark.createDataFrame(df), "longevity", "atg_code")
+
+# COMMAND ----------
+
+count_pivot_table(spark.createDataFrame(df), "price_value", "atg_code")
 
 # COMMAND ----------
 
